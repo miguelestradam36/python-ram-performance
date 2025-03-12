0b393b85f566c7a03268f4c6712a847d0631fa06
miguelestradam36 <miguelestradam36@gmail.com>
2025-03-11 21:10:21 -0600

    Uploading structure

---
+++ cpu_module/cpu_manager.py
@@ -0,0 +1,7 @@
+class PCManager:
+    # Attributes
+    psutil_ = __import__("psutil")
+    # Methods
+    def __init__(self):
+    def print_ram_performance(self)->None:
+    def __del__(self):
---
+++ main.py
@@ -0,0 +1,3 @@
+from cpu_module.cpu_manager import PCManager
+
+if __name__ == "__main__":
