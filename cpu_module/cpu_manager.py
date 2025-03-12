5db2710eb421fc6a282da98300bd6fc9f61c0f89
miguelestradam36 <miguelestradam36@gmail.com>
2025-03-11 21:29:10 -0600

    CPU percentage method added

--- cpu_module/cpu_manager.py
+++ cpu_module/cpu_manager.py
@@ -3,5 +3,18 @@ class PCManager:
     psutil_ = __import__("psutil")
     # Methods
     def __init__(self):
-    def print_ram_performance(self)->None:
+        print("Starting the PC analysis...")
+    def print_cpu_usage(self, seconds:int)->str:
+        """
+        Prints the CPU percentage used in x amount of seconds
+        """
+        try:
+            # Calling psutil.cpu_precent() for x seconds
+            return 'The CPU usage is: {}%, in {} seconds of analysis.'.format(self.psutil_.cpu_percent(seconds), seconds)
+        except Exception as error:
+            return "ERROR: {}".format(error)
+    def print_ram_performance(self)->str:
+        """
+        """
     def __del__(self):
+        print("Finished the PC analysis...")
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from cpu_module.cpu_manager import PCManager
 
 if __name__ == "__main__":
+    object_ = PCManager()
+    # CPU percentage usage in 10 seconds
+    print(object_.print_cpu_usage(seconds=10))
+    #
+
+    #
+
+    #
+
+    #
